3a178a618b464da3cfa9e4a8ac3a926fa43c1d42
Surakshith <surakshith1904@gmail.com>
2026-03-11 20:57:26 +0530

    created activetaskcard component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import ActiveTaskCard from './ActiveTaskCard';
 
 function App() {
   const [tasks, setTasks] = useState([]);
